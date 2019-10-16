aac60e0602891c11865e3bb98acf4c365b4cecb5
SG4YK <sg4yk@outlook.com>
2019-10-16 21:30:48 +0800

    update

---
+++ Java/Easy/83.remove-duplicates-from-sorted-list.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=83 lang=java
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (43.51%)
+ * Likes:    950
+ * Dislikes: 87
+ * Total Accepted:    372K
+ * Total Submissions: 854.5K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->1->2
+ * Output: 1->2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.next.val == p.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end
---
+++ Java/Easy/88.merge-sorted-array.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ *
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (37.05%)
+ * Likes:    1349
+ * Dislikes: 3156
+ * Total Accepted:    431.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
+ * one sorted array.
+ * 
+ * Note:
+ * 
+ * 
+ * The number of elements initialized in nums1 and nums2 are m and n
+ * respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to
+ * m + n) to hold additional elements from nums2.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * 
+ * Output: [1,2,2,3,5,6]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        int[] merged = Arrays.copyOf(nums1, m+n);
+        Arrays.sort(merged);
+        for (int i = 0; i < m+n; i++) {
+            nums1[i] = merged[i];
+        }
+    }
+}
+// @lc code=end
